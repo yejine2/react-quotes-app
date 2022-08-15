e803e4e0d3370256657aea3acf7a763e89ebf6e7
yejine2 <yejine2@naver.com>
2022-08-15 19:35:14 +0900

    feat: Add redirect route & Modify path

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Switch } from "react-router-dom";
+import { Redirect, Route, Switch } from "react-router-dom";
 
 import AllQuotes from "./pages/AllQuotes";
 import QuoteDetail from "./pages/QuoteDetail";
@@ -7,10 +7,13 @@ import NewQuote from "./pages/NewQuote";
 function App() {
   return (
     <Switch>
+      <Route path="/" exact>
+        <Redirect to="/quotes" />
+      </Route>
       <Route path="/quotes" exact>
         <AllQuotes />
       </Route>
-      <Route path="/quotes/:quotesId">
+      <Route path="/quotes/:quoteId">
         <QuoteDetail />
       </Route>
       <Route path="/new-quote">
