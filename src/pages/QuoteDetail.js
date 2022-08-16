6ec3d7674743fbd0995625a42d4f84b1cbaa7aa9
yejine2 <yejine2@naver.com>
2022-08-16 22:53:25 +0900

    feat: Update quote detail

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -2,13 +2,26 @@ import { Fragment } from "react";
 import { useParams, Route } from "react-router-dom";
 
 import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "yj", text: "Learning React is fun!" },
+  { id: "q2", author: "yejin", text: "Learning React is great!" },
+];
 
 const QuoteDetail = () => {
   const params = useParams();
 
+  // dummuy quotes중 하나를 식별하고 해당 데이터를 output 할 수 있음
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+  if (!quote) {
+    return <p>No quote found!</p>;
+  }
+
   return (
     <Fragment>
-      <h1>Quote Detail page</h1>
+      <HighlightedQuote text={quote.text} author={quote.author} />
       <p>{params.quoteId}</p>
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
